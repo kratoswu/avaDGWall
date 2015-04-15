c797ea9c62a0425227b72d74e01c08c40b156ad5
Cheng-Yen Tsai <yen.tsai@avadesign.com.tw>
2015-04-15 12:23:50 +0800

    追加 RTSP 的 activity, 目前暫時先不動

--- avaDGWall/src/com/avadesign/RTSPVideoSettingActivity.java
+++ avaDGWall/src/com/avadesign/RTSPVideoSettingActivity.java
@@ -1,11 +1,36 @@
 package com.avadesign;
 
+import com.avadesign.util.StringUtil;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class RTSPVideoSettingActivity extends Activity {
+
+    private EditText urlField;
+
+    public void doSave(View v) {
+        if (!validateURL()) {
+            // TODO show dialog
+        } else {
+            // TODO save
+        }
+    }
+
+    public void doCancel(View v) {
+        backSettingMain();
+    }
+
+    private boolean validateURL() {
+        String url = urlField.getText().toString();
+
+        return !StringUtil.isEmptyString(url);
+    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.door, menu);
@@ -18,10 +43,27 @@ public class RTSPVideoSettingActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_exit) {
-            startActivity(new Intent(this, SettingMainScreenActivity.class));
-            finish();
+            backSettingMain();
         }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void backSettingMain() {
+        startActivity(new Intent(this, SettingMainScreenActivity.class));
+        finish();
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rtsp_video_setting);
+
+        /*
+         * rtsp://admin:admin@192.168.3.33:80/live/h264/VGA
+         * rtsp://192.168.1.122/cam1/h264
+         * */
+        urlField = (EditText) findViewById(R.id.urlField);
+        urlField.setText("rtsp://192.168.1.122/cam1/h264");
     }
 
 }
